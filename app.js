5ed7003617123608107e6acf380fbcf40802b165
Samkiroko <kirokodev@gmail.com>
2020-07-09 00:50:18 +0300

    create a book class

---
+++ app.js
@@ -0,0 +1,33 @@
+// Book Class: Represents a book
+class Book {
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
+}
+// UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: "Book One",
+        author: "John Myles",
+        pages: "450",
+      },
+      {
+        title: "Book two",
+        author: "John kiroko",
+        pages: "350",
+      },
+    ];
+  }
+}
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+
+// Event: Add a Book
+
+// Event: Remove a book
